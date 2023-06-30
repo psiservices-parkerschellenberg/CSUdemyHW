4b2aef013c42977ae00ea4bcb2ee3e6826b53106
Parker Schellenberg <parker.schellenberg@psionline.com>
2023-06-30 15:57:31 -0500

    Primitive Value vs Non Primitive Reference

--- CSUdemyHW/Program.cs
+++ CSUdemyHW/Program.cs
@@ -14,6 +14,21 @@ namespace CSUdemyHW
 
             int result = Calculator.Add(1, 2);
             Console.WriteLine(result);
+
+            int[] numbers = new int[5] {1,2,3,4,5};
+            Console.WriteLine(numbers[0]);
+
+            // Check if string is a primitive type
+            bool isStringPrimitive = typeof(string).IsPrimitive;
+            Console.WriteLine("Is string a primitive type? " + isStringPrimitive);
+
+            // Check if int is a primitive type
+            bool isIntPrimitive = typeof(int).IsPrimitive;
+            Console.WriteLine("Is int a primitive type? " + isIntPrimitive);
+
+            // Check if StringBuilder (reference type) is a primitive type
+            bool isStringBuilderPrimitive = typeof(System.Text.StringBuilder).IsPrimitive;
+            Console.WriteLine("Is StringBuilder a primitive type? " + isStringBuilderPrimitive);
         }
     }
 }
